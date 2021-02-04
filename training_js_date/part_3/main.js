d852dad8bba9255d72b9b23ab3df290281fbd4a5
David Bouscarle <d.bouscarle@gmail.com>
2021-02-04 16:14:49 +0100

    date locale et format local

---
+++ training_js_date/part_3/main.js
@@ -0,0 +1,25 @@
+// Je récupère la date actuelle universelle
+
+let dateActuelle = new Date();
+
+// Je transforme la date universelle en date locale
+
+let dateLocale = dateActuelle.toLocaleString( /*'fr-FR'*/ navigator.language, {
+    weekday: 'long', // short (3 premières lettres), narrow (première lettre), long (toutes les lettres)
+    year: 'numeric', // numeric (xxxx), 2-digit (xx)
+    month: 'long', // short, narrow, long
+    day: 'numeric', // numeric, 2-digit
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+});
+
+console.log(dateLocale);
+
+// Plusieurs méthodes sont disponibles
+
+// toLocaleDateString   = jour, mois, année
+// toLocaleTimeString   = heure
+// toLocaleString        = jour, mois, année, heure
+
+// On peut récupérer la localité du navigatuer de l'utilisateur avec la méthode navigator.language, si on le passe en paramètre de notre objet date, c'est magique...
