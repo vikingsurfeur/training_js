ab4a97eba8e51427e05595e9cb85d95a8397d570
David Bouscarle <d.bouscarle@gmail.com>
2021-02-05 09:35:58 +0100

    Pull

--- training_js_object/part_3/main.js
+++ training_js_object/part_3/main.js
@@ -1,10 +1,10 @@
 // Call et Apply permettent d'inviquer immédiatement une fonction
 // Call
 
-let gandalf = { titre: 'Magicien'};
+let gandalf = { titre: 'Magicien' };
 
-function speak() {
-    console.log('Bonjour, je suis un ' + this.titre);
+function speak(arme, degats) {
+    console.log('Bonjour, je suis un ' + this.titre + ', vous possédez ' + arme + ' qui donne ' + degats + ' points de dégats.');
 }
 
-speak.apply(gandalf);
+speak.apply(gandalf, ['un baton', 75]);
